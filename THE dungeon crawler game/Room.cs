7dbb8b38afc577977066f9e81f562c2f819a1360
Samsonicus <thor_v_terp@hotmail.com>
2018-11-02 12:37:28 +0100

    Created basic Room and GameMap

---
+++ THE dungeon crawler game/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THE_dungeon_crawler_game
+{
+    class Room
+    {
+        private GameObject[,] roomTiles;
+        public Room()
+        {
+            roomTiles = new GameObject[16, 12];
+
+        }
+    }
+}
