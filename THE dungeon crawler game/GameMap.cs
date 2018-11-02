6693e0618ab8673554c18b437d9a16e3327972b1
Samsonicus <thor_v_terp@hotmail.com>
2018-11-02 13:38:06 +0100

    Added simple map and room genration

--- THE dungeon crawler game/GameMap.cs
+++ THE dungeon crawler game/GameMap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace THE_dungeon_crawler_game
 {
@@ -11,24 +12,45 @@ namespace THE_dungeon_crawler_game
     {
         private int mapWidth;
         private int mapHeight;
-        private Room[,] gameMap;
+        private Room[,] roomsLayout;
+        public Room[,] RoomsLayout
+        {
+            get
+            {
+                return roomsLayout;
+            }
+        }
+
 
         public GameMap()
         {
             mapWidth = 5;
             mapHeight = 5;
-            gameMap = new Room[mapHeight, mapWidth];
+            roomsLayout = new Room[mapHeight, mapWidth];
+            GenerateRooms();
         }
 
         public GameMap(int mapWidth,int mapHeight)
         {
             this.mapWidth = mapWidth;
             this.mapHeight = mapHeight;
-            gameMap = new Room[mapHeight, mapWidth];
+            roomsLayout = new Room[mapHeight, mapWidth];
+            GenerateRooms();
         }
         public static GameMap GenerateMap()
         {
             return new GameMap();
+        }
+
+        private void GenerateRooms()
+        {
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    roomsLayout[i, j] = Room.Generate(new Point(i, j));
+                }
+            }
         }
     }
 
