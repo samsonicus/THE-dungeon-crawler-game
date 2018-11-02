1e2c169702da6893e0323216f212faa262fd8d1f
Samsonicus <thor_v_terp@hotmail.com>
2018-11-02 13:38:06 +0100

    Added simple map and room genration

--- THE dungeon crawler game/Room.cs
+++ THE dungeon crawler game/Room.cs
@@ -10,14 +10,24 @@ namespace THE_dungeon_crawler_game
     enum RoomType { StartRoom,NormalRoom,ExitRoom}
     class Room
     {
-        private RoomType
+        private RoomType roomType = RoomType.NormalRoom;
         private Point roomMapPos;
         private GameObject[,] roomTiles;
+
+        /// <summary>
+        /// Room constructor
+        /// </summary>
+        /// <param name="roomMapPos">The posistion of the room in the game map</param>
         public Room(Point roomMapPos)
         {
             roomTiles = new GameObject[16, 12];
             this.roomMapPos = roomMapPos;
 
+        }
+
+        public static Room Generate(Point roomMapPos)
+        {
+            return new Room(roomMapPos); 
         }
     }
 }
