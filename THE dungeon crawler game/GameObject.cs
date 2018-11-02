5e73dd4362d04ba489394401ed60a2441a8ec0c4
Samsonicus <thor_v_terp@hotmail.com>
2018-11-02 11:18:41 +0100

    Created basic GameObject

---
+++ THE dungeon crawler game/GameObject.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THE_dungeon_crawler_game
+{
+    class GameObject
+    {
+
+
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
