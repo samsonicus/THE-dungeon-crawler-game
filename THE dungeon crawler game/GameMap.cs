2cab52761c1ef2a72a830ebb9c4322f8123ae6eb
Samsonicus <thor_v_terp@hotmail.com>
2018-11-02 13:02:46 +0100

    Contenued work on GameMap and Room

--- THE dungeon crawler game/GameMap.cs
+++ THE dungeon crawler game/GameMap.cs
@@ -9,5 +9,27 @@ namespace THE_dungeon_crawler_game
 
     class GameMap
     {
+        private int mapWidth;
+        private int mapHeight;
+        private Room[,] gameMap;
+
+        public GameMap()
+        {
+            mapWidth = 5;
+            mapHeight = 5;
+            gameMap = new Room[mapHeight, mapWidth];
+        }
+
+        public GameMap(int mapWidth,int mapHeight)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            gameMap = new Room[mapHeight, mapWidth];
+        }
+        public static GameMap GenerateMap()
+        {
+            return new GameMap();
+        }
     }
+
 }
--- THE dungeon crawler game/Room.cs
+++ THE dungeon crawler game/Room.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,16 @@ using System.Threading.Tasks;
 
 namespace THE_dungeon_crawler_game
 {
+    enum RoomType { StartRoom,NormalRoom,ExitRoom}
     class Room
     {
+        private RoomType
+        private Point roomMapPos;
         private GameObject[,] roomTiles;
-        public Room()
+        public Room(Point roomMapPos)
         {
             roomTiles = new GameObject[16, 12];
+            this.roomMapPos = roomMapPos;
 
         }
     }
