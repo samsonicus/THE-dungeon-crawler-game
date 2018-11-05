7a769f42ac625f055b029f5721aa0ccfd0d914d2
DESKTOP-F8G3CN8\Lucas <lucas@romanini.dk>
2018-11-05 11:41:58 +0100

    Gameobject done (?)

--- THE dungeon crawler game/GameObject.cs
+++ THE dungeon crawler game/GameObject.cs
@@ -13,6 +13,7 @@ namespace THE_dungeon_crawler_game
     {
         protected Texture2D sprite;
         protected Vector2 position;
+        protected float rotation;
         public Vector2 Position { get => position; }
 
         Rectangle[] animationRectangles;
@@ -22,9 +23,11 @@ namespace THE_dungeon_crawler_game
         double timeElapsed = 0;
 
         protected ContentManager content;
+        private Vector2 zero;
+        private string spriteName;
 
 
-
+        #region collision
         /// <summary>
         /// CollisionBox for the Gameobject. Height and Width are based on the height and width of the sprite used. X and Y coordinates are based on the height and width of the sprite aswell.
         /// </summary>
@@ -46,7 +49,16 @@ namespace THE_dungeon_crawler_game
             return CollisionBox.Intersects(otherObject.CollisionBox);
         }
 
+        #endregion
 
+        /// <summary>
+        /// The default constructor for a gameobject
+        /// </summary>
+        /// <param name="spriteName">The name of the sprite used for the GameObject</param>
+        public GameObject(string spriteName) : this(Vector2.Zero, spriteName)
+        {
+            //this.content = Gameworld.ContentManager;
+        }
 
         /// <summary>
         /// The constructor for a GameObject without animations. 
@@ -55,23 +67,25 @@ namespace THE_dungeon_crawler_game
         /// <param name="content">A referance for the ContentManager for loadingresources</param>
         /// <param name="spriteName">The name of the texture used for the GameObject</param>
         /// <exception cref="Microsoft.Xna.Framework.Content.ContentLoadException">Thrown if a matching texture cant be found for spriteName</exception>
-        public GameObject(Vector2 starPosition, ContentManager content, string spriteName)
+        public GameObject(Vector2 starPosition, string spriteName)
         {
             position = starPosition;
-            sprite = content.Load<Texture2D>(spriteName);
+            sprite = Gameworld.ContentManager.Load<Texture2D>(spriteName);
         }
-
-
-
-
 
         public virtual void Update(GameTime gameTime)
         {
 
         }
 
+        /// <summary>
+        /// Standart draw function for game objects. SImply draws the sprite given to it.
+        /// </summary>
+        /// <param name="spriteBatch">The spritebatch used for the drawing</param>
+        /// <param name="gameTime"></param>
         public virtual void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
+            spriteBatch.Draw(sprite, position, null, Color.White, rotation, new Vector2(sprite.Width * 0.5f, sprite.Height * 0.5f),1f, new SpriteEffects(),0f);
 
         }
     }
