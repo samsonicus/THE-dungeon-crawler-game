917ca2f53b31a66d25805dff5027248042b35a9a
SindriBerg <40951415+SindriBerg@users.noreply.github.com>
2018-11-05 13:05:03 +0100

    Pre rebase commit

---
+++ THE dungeon crawler game/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace THE_dungeon_crawler_game
+{
+    class Entity : GameObject
+    {
+        private int speed;
+        private Vector2 direction;
+        List<GameObject> gameObject;
+
+
+        virtual protected void Die()
+        {
+         
+            
+        }
+
+        public void Move()
+        {
+
+
+
+        }
+
+        public void Generate(Vector2 startPosition, string spriteName)
+        {
+
+
+
+        }
+
+
+
+    }
+}
