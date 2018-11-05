ff96f02c51463edf0fd88c3a29fc757aafc4df4a
Samsonicus <thor_v_terp@hotmail.com>
2018-11-05 12:01:34 +0100

    Added Gameobject inheritence and constructor

--- THE dungeon crawler game/Tiles.cs
+++ THE dungeon crawler game/Tiles.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace THE_dungeon_crawler_game
 {
-    class Tiles
+    class Tiles : GameObject
     {
+        public Tiles(Vector2 starPosition, string spriteName) : base(starPosition, spriteName)
+        {
+        }
     }
 }
