76dc779b71d979ab5feb678000f99743f5857ac5
DESKTOP-F8G3CN8\Lucas <lucas@romanini.dk>
2018-11-05 13:22:15 +0100

    Interface Icollidable & ICombatEntity added

---
+++ THE dungeon crawler game/ICollidable.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THE_dungeon_crawler_game
+{
+    interface ICollidable
+    {
+        Rectangle CollisionBox();
+        bool IsColliding();
+        void DoCollision();
+    }
+}
---
+++ THE dungeon crawler game/ICombatEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THE_dungeon_crawler_game
+{
+    interface ICombatEntity
+    {
+        void Attack();
+        void GainHealth();
+        void LoseHealth();
+
+    }
+}
