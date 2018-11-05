a5822bbf5346e45fda0a357c58dcd4cd11826f9c
Samsonicus <thor_v_terp@hotmail.com>
2018-11-05 11:42:46 +0100

    Added code comment to Tiles class

--- THE dungeon crawler game/Tiles.cs
+++ THE dungeon crawler game/Tiles.cs
@@ -10,8 +10,16 @@ namespace THE_dungeon_crawler_game
 {
     class Tiles : GameObject
     {
+        /// <summary>
+        /// Constructor for the Tiles class.
+        /// </summary>
+        /// <param name="starPosition">The position of the tile.</param>
+        /// <param name="content">TODO Remove later.</param>
+        /// <param name="spriteName">The string of the spritetexture name.</param>
         public Tiles(Vector2 starPosition, ContentManager content, string spriteName) : base(starPosition, content, spriteName)
         {
         }
+
+
     }
 }
