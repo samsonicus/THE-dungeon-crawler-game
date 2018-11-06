5eef055a9640902ad6e01b458a3cf10482ca9d9a
Samsonicus <thor_v_terp@hotmail.com>
2018-11-06 11:30:19 +0100

    +ObstacleTile

---
+++ THE dungeon crawler game/ObstacleTile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace THE_dungeon_crawler_game
+{
+    class ObstacleTile : Tiles, ICollidable
+    {
+        public ObstacleTile (Vector2 starPosition, string spriteName) : base(starPosition, spriteName)
+        {
+
+        }
+
+        public void DoCollision()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsColliding()
+        {
+            throw new NotImplementedException();
+        }
+
+        Rectangle ICollidable.CollisionBox()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
