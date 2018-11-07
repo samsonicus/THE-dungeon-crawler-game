eaf6a55b5146c3bcac1ebb324c3ea87a51fea676
SindriBerg <40951415+SindriBerg@users.noreply.github.com>
2018-11-07 12:26:56 +0100

    Few changes to projectile and enemy.

--- THE dungeon crawler game/Projectile.cs
+++ THE dungeon crawler game/Projectile.cs
@@ -9,8 +9,9 @@ namespace THE_dungeon_crawler_game
 {
     class Projectile : Entity
     {
-        private const float movementSpeed = 200;
-
+        private const float movementSpeed = 100;
+        private Entity owner;
+        private int damage;
         /// <summary>
         /// Default constructor for projectile.
         /// </summary>
@@ -20,13 +21,42 @@ namespace THE_dungeon_crawler_game
         /// <param name="spriteName">Name of sprite</param>
         /// <param name="speed">Speed is set to movementSpeed, which is a constant.</param>
         /// <param name="direction">Direction of projectile</param>
-        public Projectile(int frameCount, float animationFPS, Vector2 startPosition, string spriteName, float speed, Vector2 direction) :
+        /// <param name="damage">Sets the damage of the projectile</param>
+        public Projectile(int frameCount, float animationFPS, Vector2 startPosition, string spriteName, float speed, Vector2 direction, int damage, Entity owner) :
             base(frameCount, animationFPS, startPosition, spriteName, speed, direction)
         {
+            this.damage = damage;
+            this.owner = owner;
             speed = movementSpeed;
-            
+            this.direction.Normalize(); //normalizes the path of the projectile
         }
 
+        private void DealDamage(GameObject target)
+        {
+            if (target is Enemy)//ICombatEntity)
+            {
+                //LoseHealth(damage);
+            }
+        }
+                                                                                    
+        public override void DoCollision(GameObject target)
+        {
+            if (target.Equals(owner))
+            {
+                return;
+            }
+
+            if (target is Enemy) //ICombatEntity
+            {
+                DealDamage();
+            }
+           
+
+            if (!GameWorld.ScreenSize.Intersects(CollisionBox))
+            {
+                GameWorld.RemoveGameObject(this);
+            }
+        }
 
         /// <summary>
         /// Update function that removes the bullet if it hits a wall. 
