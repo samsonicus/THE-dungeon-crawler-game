63e4023bb96a4655600b48401de1c888eaaa717b
Samsonicus <thor_v_terp@hotmail.com>
2018-11-07 11:08:41 +0100

    +Enemy

---
+++ THE dungeon crawler game/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THE_dungeon_crawler_game
+{
+    class Enemy
+    {
+    }
+}
