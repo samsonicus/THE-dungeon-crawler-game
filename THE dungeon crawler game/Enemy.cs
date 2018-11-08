54f3b75744709cafec3c234be3e263ec04c3fec8
Samsonicus <thor_v_terp@hotmail.com>
2018-11-08 11:27:13 +0100

    Som changes to ICombatEntity and done with Enemy.LookAtPlayer + som minor changes to entity

--- THE dungeon crawler game/Enemy.cs
+++ THE dungeon crawler game/Enemy.cs
@@ -7,29 +7,76 @@ using Microsoft.Xna.Framework;
 
 namespace THE_dungeon_crawler_game
 {
-    class Enemy:Entity
+    class Enemy : Entity, ICombatEntity
     {
-        private int health = 100;
-        private Vector2 direction;
-
-        public int Health { get => health; }
-
-        public Enemy(int frameCount, float animationFPS, Vector2 starPosition, string spriteName, float speed, Vector2 direction) : 
-            base(frameCount, animationFPS, starPosition, spriteName, speed, direction)
+        protected int health;        
+        public int Health
         {
-            this.direction = direction;
+            get
+            {
+                return health;
+            }
+            protected set
+            {
+                if(value < 0)
+                {
+                    health = 0;
+                    Die();
+                    return;
+                }
+                health = value;
+            }
+        }
+
+        public Enemy(int health,string spriteName, Vector2 position, float speed, Vector2 direction) : base(spriteName, position, speed, direction)
+        {
+            this.health = health;
+        }
+
+        public Enemy(int health, int frameCount, float animationFPS, Vector2 starPosition, string spriteName, float speed, Vector2 direction) : base(frameCount, animationFPS, starPosition, spriteName, speed, direction)
+        {
+            this.health = health;
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-
-            
-
-
         }
 
+        /// <summary>
+        /// Sets the direction to point at player
+        /// </summary>
+        /// <param name="playerPos"></param>
+        private void LookAtPlayer(Vector2 playerPos)
+        {
+            direction = playerPos - position;
+            direction.Normalize();
+        }
 
+        /// <summary>
+        /// Does the attack of the enemy
+        /// </summary>
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
 
+        /// <summary>
+        /// Gains the enemy life
+        /// </summary>
+        /// <param name="lifeGained">The amount of life the enemy gains</param>
+        public void GainHealth(int lifeGained)
+        {
+            Health += lifeGained;
+        }
+
+        /// <summary>
+        /// Loses the enemy life
+        /// </summary>
+        /// <param name="lifeLost">The amount of life the enemy loses</param>
+        public void LoseHealth(int lifeLost)
+        {
+            Health -= lifeLost;
+        }
     }
 }
--- THE dungeon crawler game/Entity.cs
+++ THE dungeon crawler game/Entity.cs
@@ -38,33 +38,16 @@ namespace THE_dungeon_crawler_game
         /// <param name="speed"></param>
         /// <param name="direction"></param>
         public Entity(int frameCount, float animationFPS, Vector2 starPosition, string spriteName, float speed, Vector2 direction) : 
-            base(frameCount, animationFPS, starPosition, spriteName)
+        base(frameCount, animationFPS, starPosition, spriteName)
         {
             this.speed = speed;
             this.direction = direction;
 
-        }
+        }      
 
-
-        /// <summary>
-        /// Enables the Entity to have defined game logic.
-        /// </summary>
-        /// <param name="gameTime">Amount of time elapsed since last Update()</param>
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            position += speed * direction * (float)gameTime.ElapsedGameTime.TotalSeconds;
-        }
-
-
-        virtual protected void Die()
+        protected virtual void Die()
         {
            
         }
-
-      
-
-        
-
     }
 }
