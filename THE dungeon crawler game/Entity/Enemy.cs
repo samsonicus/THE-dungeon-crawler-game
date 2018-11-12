84d432ff51bab6571cc5326886698ee2c7607b16
Samsonicus <thor_v_terp@hotmail.com>
2018-11-12 13:21:40 +0100

    Added enemy vision range and did some cleanup

--- THE dungeon crawler game/Entity/Enemy.cs
+++ THE dungeon crawler game/Entity/Enemy.cs
@@ -11,6 +11,7 @@ namespace THE_dungeon_crawler_game
 
     public class Enemy : Entity, ICombatEntity
     {
+        protected const int visionRange = 700;
         protected Player player;
         protected float lastAttack;
         protected Faceing faceing = Faceing.North;
@@ -27,7 +28,7 @@ namespace THE_dungeon_crawler_game
             }
             protected set
             {
-                if (value < 0)
+                if (value <= 0)
                 {
                     health = 0;
                     Die();
@@ -38,17 +39,17 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Contructor for Enemy without animation
+        /// Contructor for Enemy without animation.
         /// </summary>
-        /// <param name="health">Enemy start health</param>
-        /// <param name="attackRange">The range at which the enemy can attack</param>
-        /// <param name="attackWidth"></param>
-        /// <param name="attackSpeed"></param>
-        /// <param name="attackDamage"></param>
-        /// <param name="spriteName">The name of the sprite</param>
-        /// <param name="position">the position at which the enemy starts at</param>
-        /// <param name="speed">The speed at which the enemy can move</param>
-        /// <param name="direction">The direction the enemy starts pointing</param>
+        /// <param name="health">Enemy start health.</param>
+        /// <param name="attackRange">The range at which the enemy can attack.</param>
+        /// <param name="attackWidth">The width of the attack.</param>
+        /// <param name="attackspeed">The amount of attack per second the enemy can perform.</param>
+        /// <param name="attackDamage">the amount of damage.</param>
+        /// <param name="spriteName">The name of the sprite.</param>
+        /// <param name="position">the position at which the enemy starts at.</param>
+        /// <param name="speed">The speed at which the enemy can move.</param>
+        /// <param name="direction">The direction the enemy starts pointing.</param>
         public Enemy(int health, float attackRange,int attackWidth ,float attackSpeed, int attackDamage, string spriteName, Vector2 position, int speed, Vector2 direction) : base(spriteName, position, speed, direction)
         {
             this.health = health;
@@ -60,18 +61,19 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Constructor for Enemy with animation
+        /// Constructor for Enemy with spritestrip animation.
         /// </summary>
-        /// <param name="health">Enemy start health</param>
-        /// <param name="attackRange">The range at which the enemy can attack</param>
-        /// <param name="attackWidth">The width of the attack</param>
-        /// <param name="attackspeed">The amount of attack per second the enemy can perform</param>
-        /// <param name="frameCount">The amount of frames in the animation</param>
-        /// <param name="animationFPS">The amount of frames to be shown in a second</param>
-        /// <param name="starPosition">the position at which the enemy starts at</param>
-        /// <param name="spriteName">The name of the sprite</param>
-        /// <param name="speed">The speed at which the enemy can move</param>
-        /// <param name="direction">The direction the enemy starts pointing</param>
+        /// <param name="health">Enemy start health.</param>
+        /// <param name="attackRange">The range at which the enemy can attack.</param>
+        /// <param name="attackWidth">The width of the attack.</param>
+        /// <param name="attackspeed">The amount of attack per second the enemy can perform.</param>
+        /// <param name="attackDamage">the amount of damage.</param>
+        /// <param name="frameCount">The amount of frames in the animation.</param>
+        /// <param name="animationFPS">The amount of frames to be shown in a second.</param>
+        /// <param name="starPosition">the position at which the enemy starts at.</param>
+        /// <param name="spriteName">The name of the sprite.</param>
+        /// <param name="speed">The speed at which the enemy can move.</param>
+        /// <param name="direction">The direction the enemy starts pointing.</param>
         public Enemy(int health, float attackRange, int attackWidth,float attackspeed,int attackDamage, int frameCount, float animationFPS, Vector2 starPosition, string spriteName, int speed, Vector2 direction) : base(frameCount, animationFPS, starPosition, spriteName, speed, direction)
         {
             this.health = health;
@@ -82,6 +84,10 @@ namespace THE_dungeon_crawler_game
             player = GameWorld.Player;
         }
 
+        /// <summary>
+        /// Gets called each frame.
+        /// </summary>
+        /// <param name="gameTime">GameTime from the game.</param>
         public override void Update(GameTime gameTime)
         {
             SimpleAI(gameTime);
@@ -89,13 +95,16 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Simpel AI that calcualtes and performs the actions of the enemy
+        /// Simpel AI that calcualtes and performs the actions of the enemy.
         /// </summary>
         protected virtual void SimpleAI(GameTime gameTime)
         {
             LookAtPlayer(player.Position);
             float playerDist = DistToPlayer(player.Position);
-            Move(gameTime);
+            if (playerDist < visionRange)
+            {
+                Move(gameTime);
+            }
             if ( playerDist <= attackRange && lastAttack + attackDelay <= gameTime.TotalGameTime.TotalSeconds)
             {
                 Attack();
@@ -104,12 +113,13 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Sets the direction to point at player
+        /// Sets the direction to point at player.
         /// </summary>
-        /// <param name="playerPos"></param>
+        /// <param name="playerPos">The position of the player the Enemy will look at.</param>
         protected void LookAtPlayer(Vector2 playerPos)
         {
             direction = playerPos - position;
+
             direction.Normalize();
             if(direction.Y < -0.5f)
             {
@@ -130,10 +140,10 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Calculates the distance between the enemy and the player
+        /// Calculates the distance between the enemy and the player.
         /// </summary>
-        /// <param name="playerPos">The position of the player</param>
-        /// <returns>Returns the distance between the player and the enemy</returns>
+        /// <param name="playerPos">The position of the player.</param>
+        /// <returns>Returns the distance between the player and the enemy.</returns>
         protected float DistToPlayer(Vector2 playerPos)
         {
             Vector2 temp = playerPos - position;
@@ -141,20 +151,19 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Moves the Enemy in the direction it's facings
+        /// Moves the Enemy in the direction it's facings.
         /// </summary>
-        /// <param name="gameTime"></param>
+        /// <param name="gameTime">GameTime object.</param>
         protected void Move(GameTime gameTime)
         {
             position += direction * Speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
         }
 
         /// <summary>
-        /// Does the attack of the enemy - TODO
+        /// Does the attack of the enemy.
         /// </summary>
         public virtual void Attack()
         {
-            
             Rectangle attackBox;
             switch (faceing)
             {
@@ -180,23 +189,26 @@ namespace THE_dungeon_crawler_game
         }
 
         /// <summary>
-        /// Gains the enemy life
+        /// Gains the enemy life.
         /// </summary>
-        /// <param name="lifeGained">The amount of life the enemy gains</param>
+        /// <param name="lifeGained">The amount of life the enemy gains.</param>
         public virtual void GainHealth(int lifeGained)
         {
             Health += lifeGained;
         }
 
         /// <summary>
-        /// Loses the enemy life
+        /// Loses the enemy life.
         /// </summary>
-        /// <param name="lifeLost">The amount of life the enemy loses</param>
+        /// <param name="lifeLost">The amount of life the enemy loses.</param>
         public virtual void LoseHealth(int lifeLost)
         {
             Health -= lifeLost;
         }
 
+        /// <summary>
+        /// Removes the Enemy from the GameWorld. 
+        /// </summary>
         protected override void Die()
         {
             GameWorld.RemoveGameObject(this);
