9ec0fd803e053346e8b252af06b6f704ef75e3db
Sindri Berg <sindrinr_1@hotmail.com>
2018-11-12 17:32:15 +0100

    Added class for powerups, and added a subclass called MovementSpeedPowerup. Added sprite for said powerup

---
+++ THE dungeon crawler game/Entity/MovementSpeedPowerup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace THE_dungeon_crawler_game
+{
+    class MovementSpeedPowerup : PowerUp, ICollidable
+    {
+
+        private int speedAmount;
+        /// <summary>
+        /// Default constructor for MovementSpeedPowerup
+        /// </summary>
+        /// <param name="startPosition">Sets position of powerup</param>
+        /// <param name="speedAmount">Determines how much speed the powerup gives</param>
+        /// <param name="duration">Determines how long the powerup lasts on the player</param>
+        public MovementSpeedPowerup(Vector2 startPosition, int speedAmount, float duration) : base(1, 1, startPosition, "SpeedPowerUp", duration)
+        {
+            this.speedAmount = speedAmount;
+        }
+
+        public Rectangle CollisionBox
+        { get
+            {
+               // return new Rectangle((int)position.X, (int)position.Y, animationRectanglesSheet[0, 0].Width, animationRectanglesSheet[0, 0].Height);
+                return new Rectangle((int)position.X, (int)position.Y, 32, 32); //Hardcoded the size of the sprite, because we don't yet have a way to retrieve the tile size
+            }
+        }
+
+
+        public override void ApplyPowerup(Player player)
+        {
+            player.Speed += speedAmount;
+        }
+
+        public override void RemovePowerup(Player player)
+        {
+            player.Speed -= speedAmount;
+        }
+
+        public bool IsColliding(ICollidable otherCollidable)
+        {
+            return otherCollidable.CollisionBox.Intersects(this.CollisionBox);
+        }
+
+        public void DoCollision(ICollidable otherCollidable)
+        {
+            if (otherCollidable is Player)
+            {
+                ((Player)otherCollidable).AddPowerUp(this);
+
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, new Rectangle((int)position.X, (int)position.Y, 32, 32), Color.White);     //Hardcoded the size here also.
+        }
+
+    }
+}
